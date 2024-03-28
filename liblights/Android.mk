08c2783e6cd9dfb4d0e0e6b6a11fde329e047939
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2024-03-28 20:31:57 +0100

    use android.mk from original loganreltexx source

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -1,23 +1,35 @@
-ifeq ($(TARGET_PROVIDES_LIBLIGHT),true)
+# Copyright (C) 2013-2016, The CyanogenMod Project
+# Copyright (C) 2017, The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 LOCAL_PATH := $(call my-dir)
+# HAL module implemenation stored in
+# hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
 
-ifneq ($(strip $(TARGET_DISPLAY_GAMMA_DISABLED)),true)
-    LOCAL_CFLAGS += -DENABLE_GAMMA_CORRECTION
+ifeq ($(BOARD_HAVE_GENERIC_BLN),true)
+    LOCAL_CFLAGS += -DGENERIC_BLN
 endif
 
-LOCAL_SRC_FILES := \
-    lights.cpp
+ifeq ($(BOARD_HAVE_MULTI_COLOR_LED),true)
+    LOCAL_CFLAGS += -DMULTI_COLOR_LED
+endif
 
-LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libutils libcutils
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_SRC_FILES := lights.c
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_MODULE := lights.msm8960
-
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif
